bab0c6e8f136d660345a7c08651d9a3dcb02c2e1
Daniel Zhang <danzhang@unc.edu>
2023-09-19 11:44:51 -0400

    edits

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -18,3 +18,21 @@ def is_equal(list_one: list[int], list_two: list[int]) -> bool:
         if num != list_two[index]:
             return False
     return True
+def only_evens(list_ints: list[int]) -> list[int]:
+    evenints = []
+    for num in list_ints:
+        if num % 2 == 0:
+            evenints.append(num)
+    return evenints
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    biglist = []
+    for num in list1:
+        biglist.append[num]
+    for num in list2:
+        biglist.append[num]
+    return biglist
+def sub(list_ints: list[int],startind:int, endind: int) -> list[int]:
+    endlist = []
+    for num in list_ints[startind:endind]:
+        endlist.append(num)
+    return endlist
--- exercises/ex05/utils_test.py
+++
@@ -1,4 +0,0 @@
-"""Docstring."""
-__author__ = '730630815'
-from exercises.ex05.utils import only_evens, sub, concat
-from exercises.ex04_utils import all, max, is_equal
