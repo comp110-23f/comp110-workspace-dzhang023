583050513181eb64cf9b60b178fe114dae16b44d
Daniel Zhang <danzhang@unc.edu>
2023-09-21 15:02:52 -0400

    trying to debug input guess?

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,6 +1,9 @@
 """Docstring."""
 __author__ = '730630815'
-def contains_char(word, letter:str) -> bool:
+
+
+def contains_char(word: str, letter: str) -> bool:
+    """Finds if a given letter is in a given word."""
     assert len(letter) == 1
     letterinword = False
     index = 0
@@ -9,7 +12,10 @@ def contains_char(word, letter:str) -> bool:
             letterinword = True
         index += 1
     return letterinword
-def emojified(guessword:str, secretword:str) -> str:
+
+
+def emojified(guessword: str, secretword: str) -> str:
+    """Gives the boxes that indicate the correctness of each guess."""
     assert len(guessword) == len(secretword)
     boxoutput = ''
     index = 0
@@ -22,24 +28,44 @@ def emojified(guessword:str, secretword:str) -> str:
             boxoutput += "\U00002B1C"
         index += 1
     return boxoutput
+
+
 def input_guess(properlen: int) -> str:
+    """Ensures that the guess is the proper length."""
     guess = input(f'Enter a {properlen} letter word: ')
-    while len(guess) != properlen:
-        guess = input(f"That wasn't {properlen} chars! Try again: ")
+    while not correct_len:
+        if len(guess) == properlen:
+            all_valid_chars = True
+            index = 0
+            while index != properlen:
+                if not guess[index].isalpha():
+                    all_valid_chars = False
+                index += 1
+            if all_valid_chars:
+                correct_len = True
+        if not correct_len:
+            guess = input(f"That wasn't {properlen} chars! Try again: ")
     return guess
+
+
 def main() -> None:
+    """Main function of code."""
     secret = 'codes'
     guess = ''
     index = 0
-    while index != 7:
+    emojified_guess = ''
+    while index != 6:
+        index += 1
         print(f'=== Turn {index}/6 ===')
         guess = input_guess(len(secret))
-        print(emojified(guess,secret))
+        emojified_guess = emojified(guess, secret)
+        print(emojified_guess)  
         if guess == secret:
             print(f"You won in {index}/6 turns!")
             break
-        index += 1
     if guess != secret:
         print('X/6 - Sorry, try again tomorrow!')
+
+
 if __name__ == "__main__":
     main()
