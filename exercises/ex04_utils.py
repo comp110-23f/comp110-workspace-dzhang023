8c0fb02cc27b14bd78d019da125f38c8576cc489
Daniel Zhang <danzhang@unc.edu>
2023-10-05 13:31:38 -0400

    edits to utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -5,7 +5,9 @@ __author__ = "730630815"
 def all(int_list: list[int], indiv_int: int) -> bool:
     """Finds if all of the numbers in a list are equal to another specified number."""
     index = 0
-    while index <= len(int_list):
+    if len(int_list) == 0:
+        return False
+    while index != len(int_list):
         if int_list[index] != indiv_int:
             return False
         index += 1
@@ -18,7 +20,7 @@ def max(int_list: list[int]) -> int:
         raise ValueError("max() arg is an empty List")
     curr_max = int_list[0]
     index = 0
-    while index <= len(int_list):
+    while index != len(int_list):
         if int_list[index] >= curr_max:
             curr_max = int_list[index]
         index += 1
@@ -29,8 +31,10 @@ def is_equal(list_one: list[int], list_two: list[int]) -> bool:
     """Finds if two provided lists are equal to one another."""
     if len(list_one) != len(list_two):
         return False
+    if len(list_one) == len(list_two) == 0:
+        return True
     index = 0
-    while index <= len(list_one):
+    while index != len(list_one):
         if list_one[index] != list_two[index]:
             return False
         index += 1
---
+++ lessons/lists and extra utils.py
@@ -0,0 +1,33 @@
+"""Practice with lists."""
+grocery_list : list[str] = list()
+grocery_list.append("bananas")
+grocery_list.append("milk")
+grocery_list.append("bread")
+grocery_list.pop(2)
+print(len(grocery_list))
+
+def only_evens(list_ints: list[int]) -> list[int]:
+    """Given a list, the function returns a list with only the even values of the given."""
+    even_ints = []
+    for num in list_ints:
+        if num % 2 == 0:
+            even_ints.append(num)
+    return even_ints
+
+
+def concat(list1: list[int], list2: list[int]) -> list[int]:
+    """Combines two given lists."""
+    big_list = []
+    for num in list1:
+        big_list.append(num)
+    for num in list2:
+        big_list.append(num)
+    return big_list
+
+
+def sub(list_ints: list[int], start_ind: int, end_ind: int) -> list[int]:
+    """Creates a subarray based on two intervals."""
+    end_list = []
+    for num in list_ints[start_ind:end_ind]:
+        end_list.append(num)
+    return end_list
