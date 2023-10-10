d5e3cac3416522bcda04981e4f06dbe354d45623
Daniel Zhang <danzhang@unc.edu>
2023-10-10 00:26:35 -0400

    added jack

--- projects/turtle_project.py
+++ projects/turtle_project.py
@@ -3,7 +3,7 @@
 __author__ = "730630816"
 
 from turtle import *
-from random import randint
+from random import randint, randrange
 
 
 def draw_leaf():
@@ -44,6 +44,17 @@ def draw_branch(start_location: int, start_y: int):
     pendown()
 
 
+def draw_top_tree():
+    """Draws the top of the tree."""
+    color("green")
+    fillcolor("green")
+    begin_fill()
+    setheading(0)
+    circle(50)
+    end_fill()
+    color("sienna")
+
+
 def draw_tree(branch_cnt: int, starting_location: int) -> None:
     """Draws the trunk and then runs a loop to draw a randomized number of branches on the trunk."""
     branch_index = 0
@@ -54,20 +65,140 @@ def draw_tree(branch_cnt: int, starting_location: int) -> None:
         branch_angle = randint(25, 60)
         setheading(90)
         forward(trunk_increment)
-        if branch_index % 2 == 0:
-            right(branch_angle)
+        if branch_index == branch_cnt - 1:
+            draw_top_tree()
         else:
-            left(branch_angle)
-        draw_branch(starting_location, ycor())
+            if branch_index % 2 == 0:
+                right(branch_angle)
+            else:
+                left(branch_angle)
+            draw_branch(starting_location, ycor())
         branch_index += 1
+
+
+def draw_plaid_box(block_color: str, girth: int, box_length):
+    fillcolor(block_color)
+    begin_fill()
+    setheading(0)
+    forward(girth/2)
+    setheading(90)
+    forward(box_length)
+    setheading(180)
+    forward(girth/2)
+    return_x = xcor()
+    return_y = ycor()
+    forward(girth/2)
+    setheading(270)
+    forward(box_length)
+    setheading(0)
+    forward(girth/2)
+    end_fill()
+    penup()
+    setpos(return_x, return_y)
+    pendown()
+
+
+def draw_neck_and_head(neck_len: int, head_rad: int):
+    color('black')
+    setheading(90)
+    forward(neck_len)
+    setheading(0)
+    circle(head_rad)
+
+
+def draw_axe(length: int, holding_angle: int):
+    fillcolor('red')
+    begin_fill()
+    width = randint(15, 30)
+    breadth = width * 2 / 3
+    setheading(holding_angle)
+    backward(length/3)
+    forward(length)
+    setheading(holding_angle-90)
+    forward(width)
+    setheading(holding_angle)
+    forward(breadth)
+    setheading(holding_angle-90)
+    backward(width)
+    setheading(holding_angle)
+    backward(breadth)
+    end_fill()
+
+
+
+def draw_arms(arm_len: int, axe_len:int, torso_size:int):
+    color("black")
+    arm_angle = randint(-75, 0)
+    return_pos_x = xcor()
+    return_pos_y = ycor()
+    penup()
+    setheading(0)
+    forward(torso_size/2)
+    setheading(arm_angle)
+    pendown()
+    forward(arm_len)
+    draw_axe(axe_len, arm_angle + 90)
+    penup()
+    setpos(return_pos_x, return_pos_y)
+    setheading(180)
+    forward(torso_size/2)
+    pendown()
+    setheading(180 - arm_angle)
+    forward(arm_len)
+    penup()
+    setpos(return_pos_x, return_pos_y)
+    pendown()
+
+
+
+def draw_lumberjack():
+    """Draws a randomly sized lumberjack."""
+    penup()
+    color("black")
+    setpos(-300, -200)
+    number_of_plaid_boxes: int = randrange(5,11, 2) 
+    leg_length: int = randint(50, 100)
+    arm_length: int = randint(35, 75)
+    torso_length: int = randint(75, 150)
+    torso_girth: int = randint(25, 50)
+    torso_increment_size: int = torso_length/number_of_plaid_boxes
+    axe_length: int = randint(75,150)
+    neck_length: int = randint(10,25)   
+    head_size: int = randint(15, 35)
+    pendown()
+    setheading(60)
+    forward(leg_length)
+    leg_base_x: int = xcor()
+    leg_base_y: int = ycor()
+    setheading(-60)
+    forward(leg_length)
+    setpos(leg_base_x,leg_base_y)
+    setheading(90)
+    index: int = 0
+    while index != number_of_plaid_boxes:
+        if index == number_of_plaid_boxes - 1:
+            draw_arms(arm_length, axe_length, torso_girth)
+        if index % 2 == 1:
+            draw_plaid_box("red", torso_girth, torso_increment_size)
+        else:
+            draw_plaid_box("black", torso_girth, torso_increment_size)
+        index += 1
+    draw_neck_and_head(neck_length, head_size)
+
+    
+
+
 
 
 def main(tree_cnt) -> None:
     """Calls the tree drawing function."""
     index = 0
     while index != tree_cnt:
-        branches = randint(5, 10)
-        draw_tree(branches, 100 * (index) - 200)
+        if index == 0:
+            draw_lumberjack()
+        else:
+            branches = randint(5, 10)
+            draw_tree(branches, 100 * (index) - 200)
         index += 1
     done()
 
