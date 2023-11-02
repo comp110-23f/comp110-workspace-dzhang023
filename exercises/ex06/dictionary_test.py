35804ab5b8c688b6fc10cc1aaeb5350846daebb9
Daniel Zhang <danzhang@unc.edu>
2023-11-01 23:52:02 -0400

    starts on dict utils

---
+++ exercises/ex06/dictionary_test.py
@@ -0,0 +1,62 @@
+"""Docstring."""
+
+__author__ = "730630815"
+
+
+from dictionary import invert, favorite_color, count, alphabetizer, update_attendance
+
+
+#invert testing#
+
+def test_invert_x():
+    pass
+
+def test_invert_y():
+    pass
+
+def test_invert_z():
+    pass
+
+#favorite color testing#
+
+def test_fave_color_x():
+    pass
+
+def test_fave_color_y():
+    pass
+
+def test_fave_color_z():
+    pass
+
+#count testing#
+
+def test_count_x():
+    pass
+
+def test_count_y():
+    pass
+
+def test_count_z():
+    pass
+
+#alphabetize testing
+
+def test_alpha_x():
+    pass
+
+def test_alpha_y():
+    pass
+
+def test_alpha_z():
+    pass
+
+#update attendance testing#
+
+def test_update_attendance_x():
+    pass
+
+def test_update_attendance_y():
+    pass
+
+def test_update_attendance_z():
+    pass
